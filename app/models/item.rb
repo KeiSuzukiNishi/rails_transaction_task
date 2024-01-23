1737eeb98bcce1de9943595c6a315d90cddb7ae6
keisuzuki <kei.nishimura0131@gmail.com>
2024-01-23 16:49:23 +0900

    init

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,6 @@
 class Item < ApplicationRecord
   validates :name, presence: true
   validates :total_quantity, presence: true
+  has_many :ordered_lists
   has_many :orders, through: :ordered_lists
 end
